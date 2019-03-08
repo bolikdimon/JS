efbdcd9c97d7fa910b6378eea77bf2c672a3e6b3
Dmitry Balianouski <bolikdimon@mail.ru>
2019-03-08 18:44:32 +0300

    Added support of context

--- store-recording/src/components/Navbar.js
+++ store-recording/src/components/Navbar.js
@@ -33,12 +33,10 @@ export default class Navbar extends Component {
     }
 }
 
-const NavWrapper = styled.nav
-`
+const NavWrapper = styled.nav`
 background: var(--mainBlue);
 .nav-link{
     color:var(--mainWhite)!important;
     font-size:1.3rem;
     text-transform:capitalize;
-}
-`;
+}`;
