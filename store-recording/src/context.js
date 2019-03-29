eda20a82f98cf62acc153b192863a652bf7aa04e
Dmitry Balianouski <bolikdimon@mail.ru>
2019-03-29 18:13:31 +0300

    Completed Product page, initial design of Details page

--- store-recording/src/context.js
+++ store-recording/src/context.js
@@ -1,35 +1,50 @@
-import React, {Component} from "react";
-import {storeProducts, detailProduct} from "./data";
+import React, { Component } from "react";
+import { storeProducts, detailProduct } from "./data";
 
 const ProductContext = React.createContext();
 // Provider
 // Consumer
 
-export default class ProductProvider extends Component {
-    state={
-        products: storeProducts,
-        detailProduct:detailProduct
-    }
+class ProductProvider extends Component {
+  state = {
+    products: [],
+    detailProduct: detailProduct
+  };
+  componentDidMount() {
+    this.setProducts();
+  }
+  setProducts = () => {
+    let tempProducts = [];
+    storeProducts.forEach(item => {
+      const singleItem = { ...item };
+      tempProducts = [...tempProducts, singleItem];
+    });
+    this.setState(() => {
+      return { products: tempProducts };
+    });
+  };
 
-    handleDetail = () => {
-        console.log("Hello from detail");
-    }
+  handleDetail = () => {
+    console.log("Hello from detail");
+  };
 
-    addToCart = () => {
-        console.log("Hello from add to cart");
-    }
+  addToCart = (id) => {
+    console.log(`Hello from add to cart. id is ${id}`);
+  };
 
-    render() {
-        return (
-            <ProductContext.Provider value={{
-                ...this.state,
-                handleDetail: this.handleDetail,
-                addToCart: this.addToCart,
-            }}>
-            {this.props.children}
-            </ProductContext.Provider>
-        )
-    }
+  render() {
+    return (
+      <ProductContext.Provider
+        value={{
+          ...this.state,
+          handleDetail: this.handleDetail,
+          addToCart: this.addToCart
+        }}
+      >
+        {this.props.children}
+      </ProductContext.Provider>
+    );
+  }
 }
 
 const ProductConsumer = ProductContext.Consumer;
